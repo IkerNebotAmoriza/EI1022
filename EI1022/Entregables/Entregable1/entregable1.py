97bc2b8f2b98a405d4b3f346ded4be39ba614ebc
Iker Nebot Amoriza <ikernebot@gmail.com>
2018-10-17 20:58:00 +0200

    Pulido Entregable1

--- EI1022/Entregables/Entregable1/entregable1.py
+++ EI1022/Entregables/Entregable1/entregable1.py
@@ -25,11 +25,13 @@ def load_lab(fichero):
         row = fich.readline().split(",")
         for c in range(cols):
             column=row[c]
-            if "n" not in column:
-                edges.append(((r,c),(r-1,c)))
+            #Si la celda no tiene pared al norte o al este se añaden los pasillos
+            if "s" not in column:
+                edges.append(((r,c),(r+1,c)))
             if "e" not in column:
                 edges.append(((r,c),(r,c+1)))
 
+    #Devolvemos el grafo construido, junto con sus dimensiones
     return UndirectedGraph (E=edges), rows, cols
 
 
@@ -38,8 +40,6 @@ def load_lab(fichero):
 
 
 def recorredor_anchura(g: UndirectedGraph, source: "T", r: int, c: int  ):
-    aristas = []
-
     #Crear e inicializar matriz
     distances = []
     for row in range(r):
@@ -49,17 +49,17 @@ def recorredor_anchura(g: UndirectedGraph, source: "T", r: int, c: int  ):
     seen = set()
 
     #Añadir el punto inicial a la cola
-
     queue.push((source,source))
     seen.add(source)
 
     while len(queue) > 0:
         u,v= queue.pop()
-
+        #Miramos los sucesores del elemento extraido de la cola
         for s in g.succs(v):
+            #Si no lo hemos visitado ya lo añadimos a los visitados y anotamos en la matriz su distancia al origen
             if s not in seen:
                 seen.add(s)
-                distances[s[0]][s[1]] = distances[v[0]][v[1]] +1 #en la posición del sucesor el antecesor mas 1
+                distances[s[0]][s[1]] = distances[v[0]][v[1]] +1 #En la posición del sucesor el antecesor mas 1
                 queue.push((v,s))
 
     return distances
@@ -70,13 +70,15 @@ def recorredor_anchura(g: UndirectedGraph, source: "T", r: int, c: int  ):
 
 
 def getDistances(g: UndirectedGraph, row: int, col: int):
+    #Declaramos las celdas de origen y final
     init=(0,0)
     fin = (row-1,col-1)
 
+    #Y mediante una llamada al recorredor de anchura recuperamos la matriz de distancias para cada origen
     inicio_fin = recorredor_anchura(g, init, row, col)
     fin_inicio = recorredor_anchura(g, fin, row, col)
 
-
+    #Devolvemos las matrices
     return inicio_fin, fin_inicio
 
 
@@ -87,9 +89,10 @@ def getDistances(g: UndirectedGraph, row: int, col: int):
 def derribar_pared(mInicio, mFinal, rows,cols):
     distMin = ( mInicio[0][0] + mFinal[0][0] )
     pared = ()
+    #Recorremos las filas y columnas del laberinto
     for row in range(rows):
         for col in range(cols):
-
+            #Y para cada una de ellas comprobamos la suma minima de distancias para sus celdas este y sur
             if(col != cols-1 and (mInicio[row][col]+mFinal[row][col+1]) < distMin):
                 distMin = (mInicio[row][col]+mFinal[row][col+1])
                 pared = ((row,col),(row,col+1))
@@ -98,6 +101,7 @@ def derribar_pared(mInicio, mFinal, rows,cols):
                 distMin = (mInicio[row][col] + mFinal [row+1][col])
                 pared = ((row+1,col),(row,col))
 
+    #Devolvemos la pared que consigue la suma minima de distancias, y la longitud de su recorrido
     return pared, distMin+1
 
 
@@ -111,15 +115,19 @@ if __name__ == '__main__':
         print("Argumentos invalidos")
         SystemExit
 
+    #Creamos el grafo del laberinto y asignamos los argumentos
     lab = load_lab(sys.argv[1])
-
     graph=lab[0]
     rows= lab[1]
     columns=lab[2]
 
-    m1,m2,recorrido= getDistances(graph,rows,columns)
+    #Creamos las matrices de distancias
+    m1,m2= getDistances(graph,rows,columns)
+
+    #Buscamos la pared que consigue el camino mas corto
     pared,distMin = derribar_pared(m1,m2,rows,columns)
 
+    #Mostramos por pantalla el resultado en el orden y formato indicados
     if pared[0] < pared [1]:
         c1 = pared[0]
         c2 = pared[1]
